9cab186b95fa4ecc30c7bd6443cad3a4b94b640e
Immanuel <purisaurabh2885@gmail.com>
2022-11-13 19:32:04 +0530

    Commit

---
+++ 1_var_let_const.js
@@ -0,0 +1,11 @@
+// var name = "vinod";
+// console.log(name);
+
+// let another = "Bahadur";
+// console.log(another);
+
+// const another_name = "Thapa";
+// console.log(another_name);
+
+
+// var is the function scope and the let and const is global scope
---
+++ 2_template_string_literals.js
@@ -0,0 +1,12 @@
+// This is can be used in ecmascript 5
+console.log("My name is Immanuel" + " My age is 20");
+
+// This can be used in ecmascript 6
+console.log(`My name is Immanuel. My age is 20`)
+
+
+// use variable in template literals
+let first_name = "Immanuel";
+let age = 20;
+
+console.log(`My name is ${first_name} and my name is ${age}`);
