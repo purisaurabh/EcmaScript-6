03d570e18d02c30731816f34af03fbf90557f4a3
Immanuel <purisaurabh2885@gmail.com>
2022-11-14 09:52:45 +0530

    Commit

--- 1_var_let_const.js
+++ 1_var_let_const.js
@@ -9,3 +9,35 @@
 
 
 // var is the function scope and the let and const is global scope
+
+
+
+
+// about var 
+
+/*
+JavaScript var keyword: The var is the oldest keyword to declare a variable in JavaScript. 
+Scope: Global scoped or function scoped. The scope of the var keyword is the global or function scope.
+It means variables defined outside the function can be accessed globally, and variables defined inside a 
+particular function can be accessed within the function. 
+*/
+
+
+
+
+// about let 
+/*
+JavaScript let keyword: The let keyword is an improved version of the var keyword. 
+Scope: block scoped: The scope of a let variable is only block scoped. 
+It can’t be accessible outside the particular block ({block}).
+*/
+
+// about const 
+/*
+const keyword in JavaScript: The const keyword has all the properties that are the 
+same as the let keyword, except the user cannot update it.
+Scope: block scoped: When users declare a const variable,
+they need to initialize it, otherwise, it returns an error. 
+The user cannot update the const variable once it is declared.
+*/
+
